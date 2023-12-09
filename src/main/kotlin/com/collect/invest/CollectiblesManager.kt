906b81955a51a0bcbd447d0c37f93bdf61500090
s.kulik <s.kulik@indevstudio.com>
2023-12-10 00:51:15 +0300

    init

--- src/main/kotlin/com/collect/invest/CollectiblesManager.kt
+++ src/main/kotlin/com/collect/invest/CollectiblesManager.kt
@@ -1,38 +1,58 @@
 package com.collect.invest
 
 import com.collect.invest.entity.CollectibleItem
-import com.collect.invest.entity.CollectibleRecord
 import com.collect.invest.utils.HttpClientFactory
 import io.ktor.http.*
-import java.time.LocalDateTime
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import java.lang.Exception
 
 class CollectiblesManager {
 
-    fun buyCollectible(item: CollectibleItem, userId: Int, sharesToBuy: Int) {
+    suspend fun buyCollectible(item: CollectibleItem, userId: Int, sharesToBuy: Int) {
         if (item.availableShares >= sharesToBuy) {
-            val totalPrice = sharesToBuy * item.currentPrice
+            val totalPrice = sharesToBuy * item.currentPrice * 1.05
             item.availableShares -= sharesToBuy
-            val record = CollectibleRecord(1, LocalDateTime.now(), sharesToBuy, item.id, userId, totalPrice, null)
-            //запись в бд
-            //update CollectibleItem в бд
 
-            println("Пользователь $userId приобрел $sharesToBuy долей ${item.name} за $totalPrice")
+            HttpClientFactory.createHttpClient().use { client ->
+                val buyResponse = client.post("http://localhost:8080/buy/$userId/$totalPrice")
+                if (buyResponse.status != HttpStatusCode.OK) {
+                    throw Exception("Error while processing buy request")
+                }
+            }
+
+            HttpClientFactory.createHttpClient().use { client ->
+                val updateResponse =
+                    client.post("http://localhost:8080/collectableService/collectable/updateCollectableById/${item.id}")
+                if (updateResponse.status != HttpStatusCode.OK) {
+                    throw Exception("Error while updating collectable")
+                }
+            }
         } else {
-            println("Недостаточно доступных долей для покупки.")
+            throw Exception("Недостаточно доступных долей для покупки.")
         }
     }
 
-    fun sellCollectible(item: CollectibleItem, userId: Int, sharesToSell: Int) {
-        val totalPrice = sharesToSell * item.currentPrice
-        item.availableShares += sharesToSell
-        val record = CollectibleRecord(1, LocalDateTime.now(), -sharesToSell, item.id, userId, totalPrice, null)
-        //запись в бд
-        //update CollectibleItem в бд
 
-        println("Пользователь $userId продал $sharesToSell долей ${item.name} за $totalPrice")
+    suspend fun sellCollectible(item: CollectibleItem, userId: Int, sharesToSell: Int) {
+        val totalPrice = sharesToSell * item.currentPrice * 0.95
+        item.availableShares += sharesToSell
+
+        HttpClientFactory.createHttpClient().use { client ->
+            val sellResponse = client.post("http://localhost:8080/sell/$userId/$totalPrice")
+            if (sellResponse.status != HttpStatusCode.OK) {
+                throw Exception("Error while processing buy request")
+            }
+        }
+
+        HttpClientFactory.createHttpClient().use { client ->
+            val updateResponse =
+                client.post("http://localhost:8080/collectableService/collectable/updateCollectableById/${item.id}")
+            if (updateResponse.status != HttpStatusCode.OK) {
+                throw Exception("Error while updating collectable")
+            }
+        }
+
     }
 
     suspend fun getCollectibleById(collectibleId: Long): CollectibleItem {
@@ -49,25 +69,26 @@ class CollectiblesManager {
                 }
             }
         }
-
-
-//        return CollectibleItem(
-//            id = 1,
-//            name = "Mocked Item",
-//            description = "Mocked Description",
-//            category = "Mocked Category",
-//            photoLink = "Mocked Photo Link",
-//            currentPrice = 100.0,
-//            availableShares = 10
-//        )
     }
 
     fun getAllCollectibles(): List<CollectibleItem> {
         // запрос бд
-        return listOf(
-            CollectibleItem(1, "Item1", "Description1", "Category1", "PhotoLink1", 50.0, 5),
-            CollectibleItem(2, "Item2", "Description2", "Category2", "PhotoLink2", 75.0, 8)
-        )
+        HttpClientFactory.createHttpClient().use { client ->
+            val response = client.get("http://localhost:8080/collectableService/collectable/getAllCollectibles")
+            when (response.status){
+                HttpStatusCode.OK -> {
+                    return response.body<List<CollectibleItem>>()
+                }
+
+                else -> {
+                    throw Exception("Error while getAllCollectibles")
+                }
+            }
+        }
+//        return listOf(
+//            CollectibleItem(1, "Item1", "Description1", "Category1", "PhotoLink1", 50.0, 5),
+//            CollectibleItem(2, "Item2", "Description2", "Category2", "PhotoLink2", 75.0, 8)
+//        )
     }
 
 }
